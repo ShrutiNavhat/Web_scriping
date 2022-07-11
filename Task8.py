08e80644e3572463ac0d9e017ade55775ef28e0f
Shruti Navhat <103164265+ShrutiNavhat@users.noreply.github.com>
2022-07-11 22:16:59 +0530

    Add files via upload

---
+++ Task8.py
@@ -0,0 +1,24 @@
+# for i in range(262,272):
+#     print(i-261)
+
+
+
+# a=[4,5,5,5,3,8]
+# i=0
+# while i<len(a)-2:
+#     if a[i]==a[i+1]==a[i+2]:
+#         print(a[i])
+#     i=i+1
+
+
+# a=[3,6,5,4,9,6,3]
+# i=0
+# # b=[]
+# # c=[]
+# while i <len(a)-2:
+#     if a[i]==a[i+1]:
+#         print(a[i])
+#     i=i+1
+# # print(b)
+
+# print(4/2*8)
